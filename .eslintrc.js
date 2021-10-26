92a87f66bd5c418890eb888a0d25f6f8460b3f13
soaresmilton <eng.milton.soares@gmail.com>
2021-10-26 16:36:47 -0300

    add aula5

---
+++ .eslintrc.js
@@ -0,0 +1,15 @@
+export default {
+    "env": {
+        "commonjs": true,
+        "es2020": true,
+        "node": true
+    },
+    "extends": [
+        "airbnb-base"
+    ],
+    "parserOptions": {
+        "ecmaVersion": 13
+    },
+    "rules": {
+    }
+};
