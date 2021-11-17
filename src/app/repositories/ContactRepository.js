e224c9ea1e26b3e85b14a3f2612544b9d38f60a1
soaresmilton <eng.milton.soares@gmail.com>
2021-11-17 15:53:40 -0300

    Add aula 21

--- src/app/repositories/ContactRepository.js
+++ src/app/repositories/ContactRepository.js
@@ -29,22 +29,20 @@ let contacts = [
 
 class ContactRepository {
   // Listar todos os registros do repositório
-  findAll() {
-    return new Promise((resolve) => resolve(contacts));
+  async findAll() {
+    const rows = await db.query('SELECT * FROM contacts');
+    return rows;
   }
 
-  findById(id) {
-    return new Promise((resolve) => resolve(
-      contacts.find((contact) => contact.id === id)
-    ));
+  async findById(id) {
+    const [row] = await db.query('SELECT * FROM contacts WHERE id = $1', [id]);
+    return row;
   }
 
-  findByEmail(email) {
-    return new Promise((resolve) => resolve(
-      contacts.find((contact) => contact.email === email)
-    ));
+  async findByEmail(email) {
+    const [row] = await db.query('SELECT * FROM contacts WHERE email = $1', [email]);
+    return row;
   }
-
 
   delete(id) {
     return new Promise((resolve) => {
